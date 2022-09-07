7e973e81c8f533e75435d6ab149519c4221fb0a1
Eduard Tatarnikov <edmaroff@gmail.com>
2022-09-07 15:32:59 +0300

    For review

--- Task3/main.py
+++ Task3/main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from pprint import pprint
+import time
+import requests
 
 
-# Press the green button in the gutter to run the script.
+class Stackoverflow:
+
+    def get_unix_time(self, quantity_day=2):
+        number_seconds = 86400 * quantity_day
+        current_date = int(time.time())
+        past_date = current_date - number_seconds
+        return [current_date, past_date]
+
+    def get_list_questions(self, tag, quantity_day):
+        url = "https://api.stackexchange.com/2.3/questions"
+        params = {
+            'tagged': tag,
+            'fromdate': self.get_unix_time()[-1],
+            'todate': self.get_unix_time()[0],
+            'site': 'stackoverflow'
+        }
+        response = requests.get(url,params=params)
+        questions_list = response.json()
+        return questions_list['items']
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    stack = Stackoverflow()
+    quantity_day = 2
+    tags = 'Python'
+    list_questions = stack.get_list_questions(tags, quantity_day)
+    print(f'Количество вопросов за последние {quantity_day} дня с тегом "{tags}" = {len(list_questions)} шт.')
+    # pprint(list_questions)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
